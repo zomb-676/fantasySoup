dbe71c41d4e52457330034e7f41d554eed0445cd
zomb_676 <zomb_676@qq.com>
2021-06-22 22:34:33 +0800

    disable something

--- src/main/java/com/github/zomb_676/fantasySoup/mixin/LoadTest.java
+++ src/main/java/com/github/zomb_676/fantasySoup/mixin/LoadTest.java
@@ -9,14 +9,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(WorldLoadProgressScreen.class)
-public class LoadTest {
-    @Inject(method = "render" , at=@At("TAIL"))
-    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks, CallbackInfo ci){
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.disableDepthTest();
-        Canvas.INSTANCE.setRGBA(255, 255, 255, 255);
-        Canvas.INSTANCE.drawFullCircle(50f, 50f, 20);
-    }
-}
+//@Mixin(WorldLoadProgressScreen.class)
+//public class LoadTest {
+//    @Inject(method = "render" , at=@At("TAIL"))
+//    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks, CallbackInfo ci){
+//        RenderSystem.enableBlend();
+//        RenderSystem.defaultBlendFunc();
+//        RenderSystem.disableDepthTest();
+//        Canvas.INSTANCE.setRGBA(255, 255, 255, 255);
+//        Canvas.INSTANCE.drawFullCircle(50f, 50f, 20);
+//    }
+//}
