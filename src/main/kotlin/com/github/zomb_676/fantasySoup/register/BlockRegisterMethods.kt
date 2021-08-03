40f246f91186e17fe5246ef5b0e921d2e57573ed
zomb_676 <zomb_676@qq.com>
2021-08-04 05:35:37 +0800

    blockEntity , render work correctly

--- src/main/kotlin/com/github/zomb_676/fantasySoup/register/BlockRegisterMethods.kt
+++ src/main/kotlin/com/github/zomb_676/fantasySoup/register/BlockRegisterMethods.kt
@@ -1,6 +1,7 @@
 package com.github.zomb_676.fantasySoup.register
 
 import com.github.zomb_676.fantasySoup.utils.BlockItemPair
+import com.github.zomb_676.fantasySoup.utils.newInstanceForEmptyOrSpecificConstructor
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockBehaviour
@@ -16,7 +17,8 @@ interface BlockRegisterMethods : ItemRegisterMethods {
     fun <T : Block> RegisterHandle.classBlock(blockClass: Class<T>, blockName: String? = null
                , blockProperty: BlockProperty? = null): RegistryObject<Block> =
         registersHolder.blockRegister.register(blockName ?: blockClass.simpleName.lowercase())
-        { Block(blockProperty?.invoke(BlockBehaviour.Properties.of(Material.STONE)) ?:
+        { blockClass.newInstanceForEmptyOrSpecificConstructor(
+            blockProperty?.invoke(BlockBehaviour.Properties.of(Material.STONE)) ?:
         BlockBehaviour.Properties.of(Material.STONE)) }
 
     @Suppress("UNCHECKED_CAST")
@@ -28,9 +30,9 @@ interface BlockRegisterMethods : ItemRegisterMethods {
 
     @Suppress("UNCHECKED_CAST")
     fun <T : Block> RegisterHandle.classBlockWithItem(blockClass: Class<T>, blockName: String? = null
-        , blockProperty: BlockProperty? = null, itemName: String?=null, itemProperty: ItemProperty?=null): BlockItemPair<Block, BlockItem> =
+        , blockProperty: BlockProperty? = null, itemName: String?=null, itemProperty: ItemProperty?=null): BlockItemPair<T, BlockItem> =
         registersHolder.blockRegister.register(blockName ?: blockClass.simpleName.lowercase())
-        { Block(blockProperty?.invoke(BlockBehaviour.Properties.of(Material.STONE)) ?:
+        { blockClass.newInstanceForEmptyOrSpecificConstructor(blockProperty?.invoke(BlockBehaviour.Properties.of(Material.STONE)) ?:
         BlockBehaviour.Properties.of(Material.STONE)) }.run { BlockItemPair(this,
             supplierItem(itemName?:blockName?:blockClass.simpleName.lowercase()
                 ,{ BlockItem(this.get(),it) },itemProperty) as RegistryObject<BlockItem>) }
