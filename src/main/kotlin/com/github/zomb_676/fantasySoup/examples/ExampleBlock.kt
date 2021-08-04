91e00daff0d23b4eb007fe15693e6717455cec60
zomb_676 <zomb_676@qq.com>
2021-08-04 15:59:22 +0800

    blockRenderType->renderShape,fix light problem for BlockEntity

--- src/main/kotlin/com/github/zomb_676/fantasySoup/examples/ExampleBlock.kt
+++ src/main/kotlin/com/github/zomb_676/fantasySoup/examples/ExampleBlock.kt
@@ -1,13 +1,17 @@
 package com.github.zomb_676.fantasySoup.examples
 
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.EntityBlock
+import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.material.Material
 
-class ExampleBlock : Block(Properties.of(Material.STONE)) , EntityBlock{
+class ExampleBlock : Block(Properties.of(Material.STONE).noOcclusion()) , EntityBlock{
+    override fun getRenderShape(arg: BlockState): RenderShape =RenderShape.ENTITYBLOCK_ANIMATED
+
     override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity = ExampleBlockEntity(pos,state)
 
 }
